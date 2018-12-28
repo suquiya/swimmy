2a3622d3684ecbfc2d0520af205a5593ec2e2b0e
suquiya <22910980+suquiya@users.noreply.github.com>
2018-12-28 11:45:05 +0900

    create init function

---
+++ swimmy.go
@@ -0,0 +1,12 @@
+/*
+swimmy is a package for fetch URL Info for embedding external site information as card or outputting as JSON.
+
+First: swimmy fetch url information (html document and text document).
+Second: swimmy sanitize html contents and parse it in order to get the information of webpage.
+*/
+
+package swimmy
+
+func init() {
+	HTTPClient = nil
+}
