f374774a72075f998dd7e4fafcc67d1d3cb0fed4
suquiya <22910980+suquiya@users.noreply.github.com>
2018-12-28 12:04:00 +0900

    add description to readme and update package comment

--- swimmy.go
+++ swimmy.go
@@ -1,9 +1,6 @@
-/*
-Package swimmy is a package for fetch URL Info for embedding external site information as card or outputting as JSON.
-
+/*Package swimmy is a package that fetch and process URL Info for embedding external site information as card or outputting as JSON.
 First: swimmy fetch url information (html document and text document).
-Second: swimmy sanitize html contents and parse it in order to get the information of webpage.
-*/
+Second: swimmy sanitize html contents and parse it in order to get the information of webpage.*/
 package swimmy
 
 func init() {
