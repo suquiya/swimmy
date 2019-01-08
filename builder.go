624f21d2711a1e6cf6cca0f62502396087e02ce0
suquiya <22910980+suquiya@users.noreply.github.com>
2019-01-09 08:37:40 +0900

    rec of impl

--- builder.go
+++ builder.go
@@ -7,35 +7,46 @@ import (
 	"io/ioutil"
 	"strconv"
 	"strings"
+	"time"
 	"unicode/utf8"
 
 	"golang.org/x/net/html/charset"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/microcosm-cc/bluemonday"
 	"golang.org/x/net/html"
 )
 
 //Sanitize sanitize html or txt with blueMonday
-func Sanitize(htmlContent string, policy *bluemonday.Policy) string {
-	return policy.Sanitize(htmlContent)
+func Sanitize(htmlContent string, policy ...*bluemonday.Policy) string {
+	if len(policy) < 1 {
+		return DefaultPageDataBuilder.PreSanitizePolicy.Sanitize(htmlContent)
+	}
+	return policy[0].Sanitize(htmlContent)
 }
 
 //PageDataBuilder is processer for creating pagedata
 type PageDataBuilder struct {
-	SanitizePolicy *bluemonday.Policy
+	PreSanitizePolicy        *bluemonday.Policy
+	TagContentSanitizePolicy *bluemonday.Policy
 }
 
+//TagContentSanitize sanitize content of tag
+func (p *PageDataBuilder) TagContentSanitize(str string) string {
+	return p.TagContentSanitizePolicy.Sanitize(str)
+}
+
+//DefaultPageDataBuilder is swimmy's default pagedatabuilder
+var DefaultPageDataBuilder *PageDataBuilder
+
 //NewPageDataBuilder generate New instance of PageDataBuilder
-func (p *PageDataBuilder) NewPageDataBuilder(custompolicy ...*bluemonday.Policy) *PageDataBuilder {
-	if len(custompolicy) < 1 {
-		return &PageDataBuilder{CPolicy()}
-	}
-	return &PageDataBuilder{custompolicy[0]}
+func NewPageDataBuilder(PrePolicy, tagContentPolicy *bluemonday.Policy) *PageDataBuilder {
+	return &PageDataBuilder{PrePolicy, tagContentPolicy}
 }
 
 //Sanitize sanitize html content with p's sanitize policy.
 func (p *PageDataBuilder) Sanitize(htmlContent string) string {
-	return Sanitize(htmlContent, p.SanitizePolicy)
+	return Sanitize(htmlContent, p.PreSanitizePolicy)
 }
 
 /*
@@ -44,7 +55,7 @@ Before parsing, Parse sanitize html content with its SanitizePolicy.
 */
 func (p *PageDataBuilder) BuildPageData(pd *PageData, htmlContent string) *PageData {
 
-	sanitizedContent := Sanitize(htmlContent, p.SanitizePolicy)
+	sanitizedContent := Sanitize(htmlContent, p.PreSanitizePolicy)
 	canTokenize := true
 	WhyCannotTokenize := ""
 	if !utf8.ValidString(sanitizedContent) {
@@ -62,7 +73,7 @@ func (p *PageDataBuilder) BuildPageData(pd *PageData, htmlContent string) *PageD
 				panic(err)
 			}
 			sanitizedContent = string(scb)
-			sanitizedContent = Sanitize(htmlContent, p.SanitizePolicy)
+			sanitizedContent = Sanitize(htmlContent, p.PreSanitizePolicy)
 		} else {
 			fmt.Printf("bad encode: %s", name)
 			canTokenize = false
@@ -114,14 +125,25 @@ func (p *PageDataBuilder) BuildPageData(pd *PageData, htmlContent string) *PageD
 							}
 						}
 
-						switch nameAttr {
-						case "":
+						p.TagContentSanitize(nameAttr)
+						p.TagContentSanitize(contentAttr)
+						switch {
+						case nameAttr == "":
 							if contentAttr != "" {
 								metaNameEmptyCount++
-								pd.Ogp.OtherAttrs["empty"+strconv.Itoa(metaNameEmptyCount)] = contentAttr
+								pd.OGP.OtherAttrs["empty"+strconv.Itoa(metaNameEmptyCount)] = contentAttr
 							}
-						case "description":
+						case nameAttr == "description":
+							pd.Description = html.EscapeString(contentAttr)
+						case nameAttr == "cannonical":
+							if govalidator.IsRequestURL(contentAttr) {
+								pd.CannonicalURL = contentAttr
+							}
+						case strings.HasPrefix(nameAttr, "og:"):
+							pd.OGP.Set(nameAttr, contentAttr)
+
 						}
+
 					}
 				case "title":
 					pd.Title = TakeMarkedUpText(cTokenizer, tnByte)
@@ -134,6 +156,33 @@ func (p *PageDataBuilder) BuildPageData(pd *PageData, htmlContent string) *PageD
 
 	return pd
 
+}
+
+//ParseTime parse time string
+func ParseTime(timeStr string) (*time.Time, string, error) {
+	formatStrings := []string{
+		"2006-01-02T15:04:05-07:00",
+		"2006-01-02T15:04:05Z",
+		"20060102T150405-0700",
+		"20060102T150405Z"}
+
+	format := ""
+	var resultTime *time.Time
+	resultTime = nil
+	var err error
+	err = nil
+	for _, fStr := range formatStrings {
+		t, err := time.Parse(fStr, timeStr)
+		if err == nil {
+			format = fStr
+			resultTime = &t
+			break
+		}
+	}
+	if resultTime == nil {
+		err = fmt.Errorf("error: cannot parse format")
+	}
+	return resultTime, format, err
 }
 
 //TakeMarkedUpText is take marked-up text between begin tag and end tag
@@ -244,4 +293,11 @@ func CPolicy() *bluemonday.Policy {
 	cp.AllowAttrs("rel", "href").OnElements("link")
 
 	return cp
+}
+
+//TPolicy return default tag policy of swimmy
+func TPolicy() *bluemonday.Policy {
+	tp := bluemonday.NewPolicy()
+
+	return tp
 }
--- pagedata.go
+++ pagedata.go
@@ -1,28 +1,108 @@
 package swimmy
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"golang.org/x/net/html"
+)
 
 //PageData is a struct for storage data(information) of web page specified with url in order to create embed card or json data
 type PageData struct {
-	URL         string
-	ContentType string
-	Title       string
-	Description string
-	FaviconURL  string
-	Image       *ImageData
-	Ogp         *OGP
+	URL           string
+	CannonicalURL string
+	ContentType   string
+	Title         string
+	Description   string
+	FaviconURL    string
+	OGP           *OpenGraphProtocol
 }
 
-//OGP is open graph protocol data
-type OGP struct {
+//OpenGraphProtocol is strage for open graph protocol. OpenGraphProtocol in swimmy is only for creating data for embedding in website, so it does not storage video and music.
+type OpenGraphProtocol struct {
 	URL          string
 	SiteName     string
 	Title        string
 	Description  string
+	Locale       string
+	Type         string
 	OgImage      *ImageData
 	TwitterImage *ImageData
 	UpdatedTime  *time.Time
 	OtherAttrs   map[string]string
+	OtherInfo    map[string]string
+}
+
+//Set set meta values to ogp fields
+func (ogp *OpenGraphProtocol) Set(nameAttr, contentAttr string) {
+	if strings.HasPrefix(nameAttr, "og:") {
+		val := strings.TrimLeft(nameAttr, "og:")
+		other := false
+		switch val {
+		case "locale":
+			ogp.Locale = html.EscapeString(contentAttr)
+		case "title":
+			ogp.Title = html.EscapeString(contentAttr)
+		case "type":
+			ogp.Type = html.EscapeString(contentAttr)
+		case "description":
+			ogp.Type = html.EscapeString(contentAttr)
+		case "url":
+			if govalidator.IsRequestURL(contentAttr) {
+				ogp.URL = contentAttr
+			}
+		case "site_name":
+			ogp.SiteName = html.EscapeString(contentAttr)
+		case "updated_time":
+			timeString := html.EscapeString(contentAttr)
+			t, f, err := ParseTime(timeString)
+			if err == nil {
+				ogp.UpdatedTime = t
+				ogp.OtherInfo["TimeFormat"] = f
+			} else {
+				fmt.Println(err)
+				ogp.UpdatedTime = nil
+				ogp.OtherInfo["TimeFormat"] = "unknown"
+				ogp.OtherInfo["UpdatedTimeString"] = "timeString"
+			}
+		default:
+			other = true
+		}
+
+		if other {
+			if strings.HasPrefix(nameAttr, "og:image") {
+				attr := strings.TrimLeft(nameAttr, "og:image")
+				switch attr {
+				case "":
+					if govalidator.IsRequestURL(contentAttr) {
+						ogp.OgImage.URL = contentAttr
+					}
+				case "secure_url":
+					if strings.HasPrefix(contentAttr, "https://") && govalidator.IsRequestURL(contentAttr) {
+						ogp.OgImage.SecureURL = contentAttr
+					}
+				case "width":
+					c := html.EscapeString(contentAttr)
+					if govalidator.IsInt(c) {
+						w, _ := strconv.Atoi(c)
+						ogp.OgImage.Width = w
+					}
+				case "height":
+					c := html.EscapeString(contentAttr)
+					if govalidator.IsInt(c) {
+						h, _ := strconv.Atoi(c)
+						ogp.OgImage.Height = h
+					}
+				case "alt":
+					ogp.OgImage.AltText = html.EscapeString(contentAttr)
+
+				}
+			}
+		}
+	}
 }
 
 //ImageData storage properties of image
@@ -30,26 +110,27 @@ type ImageData struct {
 	URL        string
 	SecureURL  string
 	FormatType string
-	width      int
-	height     int
+	AltText    string
+	Width      int
+	Height     int
 }
 
-//NewImageData return new instance of ImageData
-func NewImageData(url, secureURL, formatType string, width, height int) *ImageData {
-	return &ImageData{url, secureURL, formatType, width, height}
+//CreateImageData return new instance of ImageData
+func CreateImageData(url, secureURL, formatType, alt string, width, height int) *ImageData {
+	return &ImageData{url, secureURL, formatType, alt, width, height}
 }
 
-//NewEmptyImageData return new initialized(emply) instance of ImageData
-func NewEmptyImageData() *ImageData {
-	return &ImageData{"", "", "", -1, -1}
+//NewImageData return new initialized(emply) instance of ImageData
+func NewImageData() *ImageData {
+	return &ImageData{"", "", "", "", -1, -1}
 }
 
 //NewPageData return new instance of PageData
 func NewPageData(url string, ctype string) *PageData {
-	return &PageData{url, ctype, "", "", "", nil, nil}
+	return &PageData{url, ctype, "", "", "", "", NewOGP()}
 }
 
 //NewOGP return new instance of OGP
-func NewOGP() *OGP {
-	return &OGP{"", "", "", "", nil, nil, nil, nil}
+func NewOGP() *OpenGraphProtocol {
+	return &OpenGraphProtocol{"", "", "", "", "", "", NewImageData(), nil, nil, make(map[string]string), make(map[string]string)}
 }
